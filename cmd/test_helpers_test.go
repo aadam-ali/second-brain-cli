9e25f851a2ec256b1ad615c7bee379e64e82122d
Aadam Ali <57071686+aadam-ali@users.noreply.github.com>
2025-08-11 21:12:29 +0100

    feat: Return useful error messages and code
    This will simplify editor integrations as we can determine whether or
    not an action is required by the exit code, meaning we can change the error message without impacting any behaviour.
    
    Additionally, I've updated the `captureStdout` test helper function to also
    capture stderr since this new functionality will output error messages
    to stderr.
    
    To maintain more control and improve testability, I've silenced the
    usage and error message outputted by Cobra in favour of printing error
    messages myself. The usage message also felt redundant given the minimal
    number of flags and arguments, and commands.

--- cmd/test_helpers_test.go
+++ cmd/test_helpers_test.go
@@ -9,20 +9,29 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func captureStdout(fn func(cmd *cobra.Command, args []string) error, cmd *cobra.Command, args []string) (string, error) {
+func captureOutput(fn func(cmd *cobra.Command, args []string) error, cmd *cobra.Command, args []string) (string, string, error) {
 	originalStdout := os.Stdout
+	originalStderr := os.Stderr
 
-	var buf bytes.Buffer
-	r, w, _ := os.Pipe()
-	os.Stdout = w
+	var bufOut bytes.Buffer
+	r1, w1, _ := os.Pipe()
+	os.Stdout = w1
+
+	var bufErr bytes.Buffer
+	r2, w2, _ := os.Pipe()
+	os.Stderr = w2
 
 	err := fn(cmd, args)
 
-	w.Close()
+	w1.Close()
 	os.Stdout = originalStdout
-	io.Copy(&buf, r)
+	io.Copy(&bufOut, r1)
 
-	return buf.String(), err
+	w2.Close()
+	os.Stderr = originalStderr
+	io.Copy(&bufErr, r2)
+
+	return bufOut.String(), bufErr.String(), err
 }
 
 func prepareEnvironment() string {
